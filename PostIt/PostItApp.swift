205918435521b9ec6bbfb26d1280f9f49d391338
Riza Ocalir <riza.ocalir@gmail.com>
2026-07-14 08:04:29 +0300

    Initial Commit

---
+++ PostIt/PostItApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostItApp.swift
+//  PostIt
+//
+//  Created by Riza Ocalir on 14.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct PostItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
